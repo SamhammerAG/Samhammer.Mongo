dba4f8af1b0f7e5594eece8ae05124af75da969a
Dat <62109015+datlam162@users.noreply.github.com>
2023-08-10 13:46:19 +0700

    SSP-4440: Introduce DatabaseCredentials and add it into mongodb options

---
+++ src/Samhammer.Mongo/DatabaseCredentials.cs
@@ -0,0 +1,13 @@
+﻿namespace Samhammer.Mongo
+{
+    public class DatabaseCredential
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string AuthDatabaseName { get; set; }
+    }
+}
--- src/Samhammer.Mongo/MongoDbOptions.cs
+++ src/Samhammer.Mongo/MongoDbOptions.cs
@@ -1,16 +1,12 @@
-﻿namespace Samhammer.Mongo
+﻿using System.Collections.Generic;
+
+namespace Samhammer.Mongo
 {
     public class MongoDbOptions
     {
         public const int MaxDatabaseNameLength = 63;
 
-        public string UserName { get; set; }
-
-        public string Password { get; set; }
-
-        public string AuthDatabaseName { get; set; }
-
-        public string DatabaseName { get; set; }
+        public List<DatabaseCredential> DatabaseCredentials { get; set; }
 
         public string DatabaseHost { get; set; }
 
